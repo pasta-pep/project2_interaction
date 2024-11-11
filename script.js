e034297e2383e07db83b2d26a39a52f910a1f73a
pasta-pep <jcw.pep@gmail.com>
2024-11-11 17:44:49 -0500

    js update
    js update

--- script.js
+++ script.js
@@ -3,13 +3,23 @@
 // }
 
 
+// const today = new Date();
+
+// const year = today.getFullYear();
+// const month = today.toLocaleString('default', { month: 'long' });
+// const day = String(today.getDate()).padStart(2,'0');
+// const formattedDate = `${month}  ${day}, ${year}`;
+
+// let clock = document.getElementById('clock');
+// clock.innerHTML = formattedDate;
+
+
 const today = new Date();
 
 const year = today.getFullYear();
-// const month= String(today.getMonth() +1).padStart(2,'0');
 const month = today.toLocaleString('default', { month: 'long' });
-const day = String(today.getDate()).padStart(2,'0');
-const formattedDate = `${month}  ${day}, ${year}`;
+const day = String(today.getDate()).padStart(2, '0');
+const formattedDate = `${month} ${day}, ${year}`;  // Fixed the spacing here
 
 let clock = document.getElementById('clock');
 clock.innerHTML = formattedDate;
