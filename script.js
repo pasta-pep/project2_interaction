c69f8b3d45c5da0252ec9590ab4386aa45675a79
pasta-pep <127078213+pasta-pep@users.noreply.github.com>
2024-11-11 17:14:03 -0500

    Add files via upload
    first push

---
+++ script.js
@@ -0,0 +1,15 @@
+function myFunction() {
+    alert("Your journal is saved :)");
+}
+
+
+const today = new Date();
+
+const year = today.getFullYear();
+// const month= String(today.getMonth() +1).padStart(2,'0');
+const month = today.toLocaleString('default', { month: 'long' });
+const day = String(today.getDate()).padStart(2,'0');
+const formattedDate = `${month}  ${day}, ${year}`;
+
+let clock = document.getElementById('clock');
+clock.innerHTML = formattedDate;
