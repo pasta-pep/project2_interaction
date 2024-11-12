02996146a5be45fe1f8331f5a92c3e69935ad91e
pasta-pep <jcw.pep@gmail.com>
2024-11-11 22:14:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const year = today.getFullYear();
 // const month= String(today.getMonth() +1).padStart(2,'0');
 const month = today.toLocaleString('default', { month: 'long' });
 const day = String(today.getDate()).padStart(2,'0');
-const formattedDate = `${month}  ${day}, ${year}`;
+const formattedDate = `${month} ${day},${year}`;
 
 let clock = document.getElementById('clock');
 clock.innerHTML = formattedDate;
