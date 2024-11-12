0a6db54fcb879ff10554a623f472620380b1f8f4
pasta-pep <jcw.pep@gmail.com>
2024-11-11 22:06:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 const today = new Date();
 
 const year = today.getFullYear();
+// const month= String(today.getMonth() +1).padStart(2,'0');
 const month = today.toLocaleString('default', { month: 'long' });
-const day = String(today.getDate()).padStart(2, '0');
-const formattedDate = `${month} ${day}, ${year}`;
+const day = String(today.getDate()).padStart(2,'0');
+const formattedDate = `${month}  ${day}, ${year}`;
 
 let clock = document.getElementById('clock');
-if (clock) {
-  clock.innerHTML = formattedDate;
-} else {
-  console.error('Element with ID "clock" not found.');
-}
-
+clock.innerHTML = formattedDate;
